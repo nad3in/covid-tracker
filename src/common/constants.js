cc452d92c2096de588c7f9c9d603c83d16bb6146
nad3in <nadinelsaeed@hotmail.com>
2022-05-06 11:07:42 +0200

    add constants and commen css

---
+++ src/common/constants.js
@@ -0,0 +1,5 @@
+export const SIGNUP_BUTTON_LABEL = 'Sign up';
+export const NOT_CURRENT_USER_TEXT = 'Not a current user? sign up'
+export const LOGIN_BUTTON_LABEL = 'Login'
+export const LOGIN_FORM_TITLE = 'Login'
+export const SIGNUP_FORM_TITLE = 'Sign up'
