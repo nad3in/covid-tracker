c5d6fabdfa1ed95836b88fd5c82c27b0a8504cf1
nad3in <nadinelsaeed@hotmail.com>
2022-05-06 11:10:12 +0200

    add users locations page

---
+++ src/pages/usersLocations.js
@@ -0,0 +1,43 @@
+import Map from '../components/googleMap';
+import { useEffect, useState } from "react";
+import { get } from "../common/apiClient"
+
+
+const UsersLocations = () => {
+    const [location, setLocation] = useState();
+    const [locations, setLocations] = useState();
+    useEffect(() => {
+        async function getLocations() {
+            const res = await get('locations')
+            if (res.ok) {
+                let data = await res.json()
+                if (data && data.length > 0) {
+                    const locations = [];
+                    data.map(user => {
+                        if (user && user.entries.length > 0) {
+                            locations.push({ name: user.name, location: user.entries[user.entries.length - 1].location, temp: user.entries[user.entries.length - 1].temp });
+                        }
+                    });
+                    console.log(locations)
+                    setLocations(locations);
+                }
+            }
+        }
+        getLocations();
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(showPosition);
+        }
+        function showPosition(position) {
+            setLocation({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+        }
+    }, []);
+    return (<>
+        <Map currentLocation={location} locations={locations} >
+            {/* <Marker key={i} position={latLng} /> */}
+        </Map>
+    </>);
+}
+export default UsersLocations;
